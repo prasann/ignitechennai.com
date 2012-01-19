b20a6deb06244c36e849a6da19e7d6b8f51fd4b0
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-01-20 02:26:13 +0800

    Added old object embedding for youtube videos as iframes are not working in some IE versions. Changed the rake file appropriately to support rails3. Ran a local migration for change of phone number from integer to string.

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100315135640) do
+ActiveRecord::Schema.define(:version => 20100317062159) do
 
   create_table "mode_of_reaches", :force => true do |t|
     t.string   "name"
@@ -27,7 +28,7 @@ ActiveRecord::Schema.define(:version => 20100315135640) do
     t.datetime "updated_at"
     t.string   "company_name"
     t.string   "type_of_attendee"
-    t.integer  "phone_no"
+    t.string   "phone_no"
   end
 
 end
